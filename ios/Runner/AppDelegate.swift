b6cac787b8b3247b797b8634d5bb1abb7281da51
naimulhassan <developernaimul00@gmail.com>
2024-08-12 18:16:40 +0600

    init

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,4 +10,18 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  @discardableResult
+  override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+
+      if userActivity.activityType == NSUserActivityTypeBrowsingWeb {
+          if let webpageURL = userActivity.webpageURL {
+              print("Received Universal Link: \(webpageURL)")
+              // You may want to handle the URL here, e.g., navigate to a specific page in your Flutter app
+              return true
+          }
+      }
+
+      return false
+  }
 }
